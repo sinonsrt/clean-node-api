deab06465d8850c326c943f40b9e6958049d5946
Lucas Marchiori <sinonsrt@hotmail.com>
2022-11-17 17:19:00 -0300

    test: add password test validation

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,7 +2,7 @@ import { MissingParamError } from "../errors/missing-param-error"
 import { SignUpController } from "./signup"
 
 describe("SignUp Controller", () => {
-  test("Should return 400 if no name if provider", () => {
+  test("Should return 400 if no name is provider", () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -19,7 +19,7 @@ describe("SignUp Controller", () => {
     )
   })
 
-  test("Should return 400 if no e-mail if provider", () => {
+  test("Should return 400 if no e-mail is provider", () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -33,6 +33,23 @@ describe("SignUp Controller", () => {
     expect(httpResponse?.statusCode).toBe(400)
     expect(httpResponse?.body).toEqual(
       new MissingParamError("Missing param: email")
+    )
+  })
+
+  test("Should return 400 if no password is provider", () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: "Derrick Austin",
+        email: "tune@enooho.me",
+        passwordConfirmation: "3931650098",
+      },
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(400)
+    expect(httpResponse?.body).toEqual(
+      new MissingParamError("Missing param: password")
     )
   })
 })
