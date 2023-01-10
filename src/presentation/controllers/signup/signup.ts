45ebc24a588cbc86de0cbf68fd868c9bfbb3965a
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-10 16:18:27 -0300

    refactor: fix all signup protocols to a index file

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,12 +1,12 @@
-import { AddAccount } from "../../domain/usecases/add-account"
-import { InvalidParamError, MissingParamError } from "../errors"
-import { badRequest, serverError } from "../helpers/http-helper"
+import { InvalidParamError, MissingParamError } from "../../errors"
+import { badRequest, serverError } from "../../helpers/http-helper"
 import {
+  AddAccount,
   Controller,
   EmailValidator,
   HttpRequest,
   HttpResponse,
-} from "../protocols"
+} from "./signup-protocols"
 
 class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
