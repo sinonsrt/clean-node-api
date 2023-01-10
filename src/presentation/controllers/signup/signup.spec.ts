7dc73f8c3559868bfc6f5f3f87adc1089b344069
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-10 16:32:10 -0300

    test: ensure signUpController return 200 if is valid data provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -25,8 +25,8 @@ const makeAddAccount = (): AddAccount => {
 
       const fakeAccount = {
         id: "valid_id",
-        name: "Alta Parker",
-        email: "nadtuncik@juj.ie",
+        name: "valid_name",
+        email: "valid_email",
         password: "valid_password",
       }
 
@@ -235,5 +235,27 @@ describe("SignUp Controller", () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse?.statusCode).toBe(500)
     expect(httpResponse?.body).toEqual(new ServerError())
+  })
+
+  test("Should return 200 if data is valid", () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        email: "valid_email",
+        password: "valid_password",
+        passwordConfirmation: "valid_password",
+      },
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse?.statusCode).toBe(200)
+    expect(httpResponse?.body).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    })
   })
 })
