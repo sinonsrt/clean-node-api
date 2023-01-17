4b935d458412eb6b809a1ffa861a97e374e04d57
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-17 15:14:57 -0300

    refactor: grou dbAccount protocols

---
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -0,0 +1,6 @@
+export { AccountModel } from "../../../domain/models/account"
+export {
+  AddAccount,
+  AddAccountModel,
+} from "../../../domain/usecases/add-account"
+export { Encrypter } from "../../protocols/encrypter"
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,9 +1,9 @@
-import { AccountModel } from "../../../domain/models/account"
 import {
+  AccountModel,
   AddAccount,
   AddAccountModel,
-} from "../../../domain/usecases/add-account"
-import { Encrypter } from "../../protocols/encrypter"
+  Encrypter,
+} from "./db-add-account-protocols"
 
 class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
