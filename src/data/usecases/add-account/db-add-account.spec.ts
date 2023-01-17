cba87ba8d00b44ab1d9fabb084d837266b6b2a5d
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-17 15:40:07 -0300

    test: should throw an error if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -107,4 +107,24 @@ describe("DbAddAccount UseCase", () => {
       password: "hashed_password",
     })
   })
+
+  test("Should throw an error if AddAccountRepository throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(addAccountRepositoryStub, "add")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    }
+
+    const promise = sut.add(accountData)
+
+    expect(promise).rejects.toThrow()
+  })
 })
