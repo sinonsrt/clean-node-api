183bba3535c7ecaa6496a563bde16774107d9624
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-23 13:54:14 -0300

    feat: ensure AccountMongoRepository return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,21 @@
+import { AddAccountRepository } from "../../../../data/protocols/add-account-repository"
+import { AccountModel } from "../../../../domain/models/account"
+import { AddAccountModel } from "../../../../domain/usecases/add-account"
+import { MongoHelper } from "../helpers/mongo-helper"
+
+class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts")
+
+    const result = await accountCollection.insertOne(accountData)
+
+    const account = {
+      id: `${result.insertedId}`,
+      ...accountData,
+    }
+
+    return account
+  }
+}
+
+export { AccountMongoRepository }
