0db9de4a8fb05448b0c52720e5155c416b589a9b
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-26 14:04:41 -0300

    test: change jest converage collect config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
   coverageDirectory: "coverage",
   preset: "@shelf/jest-mongodb",
   testEnvironment: "node",
