f818869a103d092757a7074e5e7a0a2a335693b6
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-30 13:42:09 -0300

    test: ensure db goes to reconnect if mongodb is down

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,23 @@
+import { MongoHelper as sut } from "./mongo-helper"
+
+describe("MongoHelper", () => {
+  beforeAll(async () => {
+    await sut.connect(`${process.env.MONGO_URL}`)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test("Should reconnect if mongodb is down", async () => {
+    let accountCollection = await sut.getCollection("accounts")
+
+    expect(accountCollection).toBeTruthy()
+
+    await sut.disconnect()
+
+    accountCollection = await sut.getCollection("accounts")
+
+    expect(accountCollection).toBeTruthy()
+  })
+})
