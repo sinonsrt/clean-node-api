c59ff2e8bc1e90cb04d0f27beecad26e7ab42d90
Lucas Marchiori <sinonsrt@hotmail.com>
2023-01-31 13:14:27 -0300

    feat: create logControllerDecorator and ensure that call controller handle method

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from "../../presentation/protocols"
+
+class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor(controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+
+    if (httpResponse.statusCode === 500) {
+      //log
+    }
+
+    return httpResponse
+  }
+}
+
+export { LogControllerDecorator }
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,8 +3,10 @@ import { EmailValidatorAdapter } from "../../utils/email-validator-adapter"
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account"
 import { BcryptAdapter } from "../../infra/criptography/bcrypt.adapter"
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account"
+import { Controller } from "../../presentation/protocols"
+import { LogControllerDecorator } from "../decorators/log"
 
-const makeSignUpController = (): SignUpController => {
+const makeSignUpController = (): Controller => {
   const salt = 12
 
   const accountRepository = new AccountMongoRepository()
@@ -12,8 +14,9 @@ const makeSignUpController = (): SignUpController => {
   const addAccount = new DbAddAccount(encrypter, accountRepository)
   const emailValidator = new EmailValidatorAdapter()
   const signUpController = new SignUpController(emailValidator, addAccount)
+  const logController = new LogControllerDecorator(signUpController)
 
-  return signUpController
+  return logController
 }
 
 export { makeSignUpController }
