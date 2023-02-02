835b905d2d28afe044c79ee2a06f256aeeaddd14
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-02 14:08:11 -0300

    feat: ensure logControllerDecoreator calls logErrorRepository

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -5,6 +5,7 @@ import { BcryptAdapter } from "../../infra/criptography/bcrypt.adapter"
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account"
 import { Controller } from "../../presentation/protocols"
 import { LogControllerDecorator } from "../decorators/log"
+import { LogErrorRepository } from "../../data/protocols/log-error-resository"
 
 const makeSignUpController = (): Controller => {
   const salt = 12
@@ -14,7 +15,11 @@ const makeSignUpController = (): Controller => {
   const addAccount = new DbAddAccount(encrypter, accountRepository)
   const emailValidator = new EmailValidatorAdapter()
   const signUpController = new SignUpController(emailValidator, addAccount)
-  const logController = new LogControllerDecorator(signUpController)
+  const logErrorRepository = new LogErrorRepository()
+  const logController = new LogControllerDecorator(
+    signUpController,
+    logErrorRepository
+  )
 
   return logController
 }
