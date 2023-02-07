caf42aa46d6d46d64d3a30fe1c8d78d662c41184
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-07 13:22:51 -0300

    refactor: create sut on log.spec

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -2,6 +2,12 @@ import { Collection } from "mongodb"
 import { MongoHelper } from "../helpers/mongo-helper"
 import { LogMongoRepository } from "./log"
 
+const makeSut = (): LogMongoRepository => {
+  const sut = new LogMongoRepository()
+
+  return sut
+}
+
 describe("Log Mongo Repository", () => {
   let errorCollection: Collection
 
@@ -19,7 +25,7 @@ describe("Log Mongo Repository", () => {
   })
 
   test("", async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError("any_error")
 
     const count = await errorCollection.countDocuments()
