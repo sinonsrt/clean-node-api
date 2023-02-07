8e39bba9ee550da7dd9f47a53ce3c5ce2cd02f1c
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-07 13:26:19 -0300

    feat: ensure app returns the error message instead error name

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -9,7 +9,13 @@ const adaptRoute = (controller: Controller) => {
 
     const httpResponse = await controller.handle(httpRequest)
 
-    res.status(httpResponse.statusCode).json(httpResponse.body)
+    if (httpResponse.statusCode === 200) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res
+        .status(httpResponse.statusCode)
+        .json({ error: httpResponse.body.message })
+    }
   }
 }
 
