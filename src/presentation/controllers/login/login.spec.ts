fbe77642e6db098853dd8ade520dd37aa150546a
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-09 17:45:49 -0300

    test: ensure LoginController returns 400 if an invalid email is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from "../../errors"
+import { InvalidParamError, MissingParamError } from "../../errors"
 import { badRequest } from "../../helpers/http-helper"
 import { EmailValidator } from "../signup/signup-protocols"
 import { LoginController } from "./login"
@@ -73,5 +73,24 @@ describe("Login Controller", () => {
     await sut.handle(httpRequest)
 
     expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com")
+  })
+
+  test("Should return 400 if and invalid email is provided", async () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    jest.spyOn(emailValidatorStub, "isValid").mockReturnValue(false)
+
+    const httpRequest = {
+      body: {
+        email: "invalid_email",
+        password: "any_password",
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse).toStrictEqual(
+      badRequest(new InvalidParamError("email"))
+    )
   })
 })
