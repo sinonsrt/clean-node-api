42f90ca6f5a33a4a4e4833f3d555d68ddec28b53
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-09 17:35:15 -0300

    refactor: create sut function

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,9 +2,19 @@ import { MissingParamError } from "../../errors"
 import { badRequest } from "../../helpers/http-helper"
 import { LoginController } from "./login"
 
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+
+  return { sut }
+}
+
 describe("Login Controller", () => {
   test("Should return 400 if no email is provided", async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -20,7 +30,7 @@ describe("Login Controller", () => {
   })
 
   test("Should return 400 if no password is provided", async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
