27ebc92afe1e4fff2d1314af0c4dcc328173a451
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-13 18:37:03 -0300

    test: ensure LoginController return 500 if authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -137,4 +137,16 @@ describe("Login Controller", () => {
 
     expect(httpResponse).toEqual(unauthorized())
   })
+
+  test("Should return 500 if Authentication throws", async () => {
+    const { sut, authenticationStub } = makeSut()
+
+    jest.spyOn(authenticationStub, "auth").mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
