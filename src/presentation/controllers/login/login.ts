c4efb94ac88e57d5868359adf8ed06318d9cf28d
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-13 18:18:11 -0300

    refactor: refact requiredFields to a validation for

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -15,15 +15,15 @@ class LoginController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredFields = ["email", "password"]
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
       const { email, password } = httpRequest.body
-
-      if (!email) {
-        return badRequest(new MissingParamError("email"))
-      }
-
-      if (!password) {
-        return badRequest(new MissingParamError("password"))
-      }
 
       const isValidEmail = this.emailValidator.isValid(email)
 
