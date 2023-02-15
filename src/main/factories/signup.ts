cc76b41954b03d166f3f23981f3ffce47cdbe288
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-15 13:45:21 -0300

    feat: create email validation and add to composite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,5 +1,4 @@
 import { SignUpController } from "../../presentation/controllers/signup/signup"
-import { EmailValidatorAdapter } from "../../utils/email-validator-adapter"
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account"
 import { BcryptAdapter } from "../../infra/criptography/bcrypt.adapter"
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account"
@@ -14,9 +13,7 @@ const makeSignUpController = (): Controller => {
   const accountRepository = new AccountMongoRepository()
   const encrypter = new BcryptAdapter(salt)
   const addAccount = new DbAddAccount(encrypter, accountRepository)
-  const emailValidator = new EmailValidatorAdapter()
   const signUpController = new SignUpController(
-    emailValidator,
     addAccount,
     makeSignUpValidation()
   )
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,25 +1,17 @@
-import { InvalidParamError } from "../../errors"
 import { badRequest, ok, serverError } from "../../helpers/http-helper"
 import {
   AddAccount,
   Controller,
-  EmailValidator,
   HttpRequest,
   HttpResponse,
   Validation,
 } from "./signup-protocols"
 
 class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
   private readonly validation: Validation
 
-  constructor(
-    emailValidator: EmailValidator,
-    addAccount: AddAccount,
-    validation: Validation
-  ) {
-    this.emailValidator = emailValidator
+  constructor(addAccount: AddAccount, validation: Validation) {
     this.addAccount = addAccount
     this.validation = validation
   }
@@ -33,12 +25,6 @@ class SignUpController implements Controller {
       }
 
       const { name, email, password } = httpRequest.body
-
-      const isValidEmail = this.emailValidator.isValid(email)
-
-      if (!isValidEmail) {
-        return badRequest(new InvalidParamError("email"))
-      }
 
       const account = await this.addAccount.add({ name, email, password })
 
