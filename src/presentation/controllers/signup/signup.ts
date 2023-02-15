7bc5e0881a98e2cc2a0cb94fd6bd49708b5dd519
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-15 13:02:42 -0300

    feat: create compareFieldValidation

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -32,11 +32,7 @@ class SignUpController implements Controller {
         return badRequest(error)
       }
 
-      const { name, email, password, passwordConfirmation } = httpRequest.body
-
-      if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError("passwordConfirmation"))
-      }
+      const { name, email, password } = httpRequest.body
 
       const isValidEmail = this.emailValidator.isValid(email)
 
