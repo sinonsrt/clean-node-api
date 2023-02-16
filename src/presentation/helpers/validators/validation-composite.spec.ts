47f5c558738faa4dce383fe241df4d2d074cfded
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-16 14:06:47 -0300

    test: ensue vlidation composite returns errors

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,20 +2,54 @@ import { MissingParamError } from "../../errors"
 import { Validation } from "./validation"
 import { ValidationComposite } from "./validation-composite"
 
+interface SutTypes {
+  sut: ValidationComposite
+  validationStubs: Validation[]
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any) {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+const makeSut = (): SutTypes => {
+  const validationStubs = [makeValidation(), makeValidation()]
+
+  const sut = new ValidationComposite(validationStubs)
+
+  return {
+    sut,
+    validationStubs,
+  }
+}
+
 describe("Validation Composite", () => {
   test("should return an error if any validation fails", () => {
-    class ValidationStub implements Validation {
-      validate(input: any) {
-        return new MissingParamError("field")
-      }
-    }
+    const { sut, validationStubs } = makeSut()
 
-    const validationStub = new ValidationStub()
-
-    const sut = new ValidationComposite([validationStub])
+    jest
+      .spyOn(validationStubs[0], "validate")
+      .mockReturnValueOnce(new MissingParamError("field"))
 
     const error = sut.validate({ field: "any_value" })
 
     expect(error).toEqual(new MissingParamError("field"))
+  })
+
+  test("should return the first error if more then on validation fails", () => {
+    const { sut, validationStubs } = makeSut()
+
+    jest.spyOn(validationStubs[0], "validate").mockReturnValueOnce(new Error())
+    jest
+      .spyOn(validationStubs[1], "validate")
+      .mockReturnValueOnce(new MissingParamError("field"))
+
+    const error = sut.validate({ field: "any_value" })
+
+    expect(error).toEqual(new Error())
   })
 })
