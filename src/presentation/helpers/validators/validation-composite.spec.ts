a89cba4cf8135e5ab268ed56ce257fdb41532c5c
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-16 14:07:56 -0300

    test: ensure validation composite not return if validation success

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -52,4 +52,12 @@ describe("Validation Composite", () => {
 
     expect(error).toEqual(new Error())
   })
+
+  test("should not return if validation succeeds", () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({ field: "any_value" })
+
+    expect(error).toBeFalsy()
+  })
 })
