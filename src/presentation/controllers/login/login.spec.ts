889daa1dd35cee17a981474f21a22086dbdfdc15
Lucas Marchiori <sinonsrt@hotmail.com>
2023-02-28 16:40:42 -0300

    refactor: move http to inside http foler

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -4,7 +4,7 @@ import {
   ok,
   serverError,
   unauthorized,
-} from "../../helpers/http-helper"
+} from "../../helpers/http/http-helper"
 import { LoginController } from "./login"
 import {
   Authentication,
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -3,7 +3,7 @@ import {
   ok,
   serverError,
   unauthorized,
-} from "../../helpers/http-helper"
+} from "../../helpers/http/http-helper"
 import { Validation } from "../signup/signup-protocols"
 import {
   Authentication,
