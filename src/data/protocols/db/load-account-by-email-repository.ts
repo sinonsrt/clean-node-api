7443a01d5b3cbee3ca1b2d6d52b84d4cf56dc073
Lucas Marchiori <sinonsrt@hotmail.com>
2023-03-02 21:17:30 -0300

    ensure dbAuth calls hashCompare with correct value

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from "../usecases/add-account/db-add-account-protocols"
+import { AccountModel } from "../../usecases/add-account/db-add-account-protocols"
 
 interface LoadAccountByEmailRepository {
   load(email: string): Promise<AccountModel>
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,17 +2,27 @@ import {
   Authentication,
   AutheticationModel,
 } from "../../../domain/usecases/authentication"
+import { HashCompare } from "../../protocols/criptography/hash-compare"
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository"
 
 class DbAuthentication implements Authentication {
   private loadAccountByEmailRepository: LoadAccountByEmailRepository
+  private hashCompare: HashCompare
 
-  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor(
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashCompare: HashCompare
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashCompare = hashCompare
   }
 
   async auth({ email, password }: AutheticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(email)
+    const account = await this.loadAccountByEmailRepository.load(email)
+
+    if (account) {
+      await this.hashCompare.compare(password, account.password)
+    }
     return ""
   }
 }
