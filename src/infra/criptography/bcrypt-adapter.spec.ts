ecfb933a9a12db4b7f74edcdd3e5ef5dfcf0eb2c
Lucas Marchiori <sinonsrt@hotmail.com>
2023-03-07 13:33:43 -0300

    test: ensure compare return false when fails

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -75,4 +75,16 @@ describe("Bcrypt Adapter", () => {
 
     expect(isValid).toBe(true)
   })
+
+  test("Should return false when compare fails", async () => {
+    const { sut } = makeSut()
+
+    jest
+      .spyOn(bcrypt, "compare")
+      .mockImplementationOnce(() => new Promise((resolve) => resolve(false)))
+
+    const isValid = await sut.compare("any_value", "any_hash")
+
+    expect(isValid).toBe(false)
+  })
 })
