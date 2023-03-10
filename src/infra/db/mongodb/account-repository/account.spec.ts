42a93b9a72151219ce9ea4bbd33b9713eec0faf2
Lucas Marchiori <sinonsrt@hotmail.com>
2023-03-09 21:45:13 -0300

    test: ensure loadByEmail return an account

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,6 +1,8 @@
+import { Collection } from "mongodb"
 import { MongoHelper } from "../helpers/mongo-helper"
 import { AccountMongoRepository } from "./account"
 
+let accountCollection: Collection
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
     await MongoHelper.connect(`${process.env.MONGO_URL}`)
@@ -11,7 +13,7 @@ describe("Account Mongo Repository", () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts")
+    accountCollection = await MongoHelper.getCollection("accounts")
     await accountCollection.deleteMany({})
   })
 
@@ -19,12 +21,12 @@ describe("Account Mongo Repository", () => {
     return new AccountMongoRepository()
   }
 
-  test("Should return an account on success", async () => {
+  test("Should return an add account on success", async () => {
     const sut = makeSut()
 
     const accountData = {
       name: "any_name",
-      email: "any_email",
+      email: "any_email@mail.com",
       password: "hashed_password",
     }
 
@@ -33,7 +35,26 @@ describe("Account Mongo Repository", () => {
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe("any_name")
-    expect(account.email).toBe("any_email")
+    expect(account.email).toBe("any_email@mail.com")
     expect(account.password).toBe("hashed_password")
+  })
+
+  test("Should return an account on loadByEmail success", async () => {
+    const sut = makeSut()
+
+    const accountData = {
+      name: "any_name",
+      email: "any_email@mail.com",
+      password: "hashed_password",
+    }
+
+    await accountCollection.insertOne(accountData)
+
+    const account = await sut.loadByEmail("any_email@mail.com")
+
+    expect(account).toBeTruthy()
+    expect(account?.name).toBe("any_name")
+    expect(account?.email).toBe("any_email@mail.com")
+    expect(account?.password).toBe("hashed_password")
   })
 })
