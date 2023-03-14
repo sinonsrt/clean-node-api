03cc7821c885fa126dc57aee7d1d4801f8025e37
Lucas Marchiori <sinonsrt@hotmail.com>
2023-03-14 20:05:57 -0300

    refactor: refactor signup route to login

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from "supertest"
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper"
 import app from "../config/app"
 
-describe("SignUp Routes", () => {
+describe("Login Routes", () => {
   beforeAll(async () => {
     await MongoHelper.connect(`${process.env.MONGO_URL}`)
   })
@@ -16,15 +16,17 @@ describe("SignUp Routes", () => {
     await accountCollection.deleteMany({})
   })
 
-  test("Should return an account on success", async () => {
-    const response = await request(app).post("/api/signup").send({
-      name: "any_name",
-      email: "any_email@email.com",
-      password: "any_password",
-      passwordConfirmation: "any_password",
-    })
+  describe("POST /signup", () => {
+    test("Should return 200 on signup", async () => {
+      const response = await request(app).post("/api/signup").send({
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      })
 
-    expect(response.body.id).toBeTruthy()
-    expect(response.status).toBe(200)
+      expect(response.body.id).toBeTruthy()
+      expect(response.status).toBe(200)
+    })
   })
 })
