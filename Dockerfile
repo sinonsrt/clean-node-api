66ad17a3afcc0d168fb96b4b0a85993d9623a553
Lucas Marchiori <sinonsrt@hotmail.com>
2023-03-27 22:36:41 -0300

    chore: configure docker compose

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,12 @@ COPY ./package.json .
 
 RUN npm install
 
-COPY ./dist ./dist
+# COPY ./dist ./dist
 
-EXPOSE 3333
+# EXPOSE 3333
 
-CMD ["npm", "start"]
+# CMD ["npm", "start"]
+
+# docker build -t image_name .
+# docker run -it image_name sh
+# docker run -p 5000:5000 imaage_name
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,7 +3,8 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 export default {
-  mongoUrl: process.env.MONGO_URL || process.env.MONGO_CLUSTER_URL,
+  mongoUrl:
+    process.env.MONGO_CLUSTER_URL || "mongodb://root:admin@mongo:27017/",
   port: process.env.PORT || 3333,
   jwtSecret: process.env.JWT_SECRET || "tj670==5H",
 }
