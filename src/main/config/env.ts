d573fb4eac7970787904cffdc828e9c73422e9f3
Lucas Marchiori <sinonsrt@hotmail.com>
2023-03-28 23:39:26 -0300

    chore: finished docker-compose config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,7 +4,7 @@ dotenv.config()
 
 export default {
   mongoUrl:
-    process.env.MONGO_CLUSTER_URL || "mongodb://root:admin@mongo:27017/",
+    process.env.MONGO_CLUSTER_URL || "mongodb://mongo:27017/clean-node-api",
   port: process.env.PORT || 3333,
   jwtSecret: process.env.JWT_SECRET || "tj670==5H",
 }
